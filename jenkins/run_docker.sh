d793c3990bc55f0b2ca0ae5d6b8a4fe25bfb13ad
alejandrojsuazohvd <als5289@g.harvard.edu>
2020-11-14 13:08:49 -0600

    Adding installation for python and yarn

--- jenkins/run_docker.sh
+++ jenkins/run_docker.sh
@@ -2,9 +2,9 @@
 docker stop money_jenkins
 docker rm -v money_jenkins
 docker build -t moneyjenkins .
-docker run --name money_jenkins -d -v /var/run/docker.sock:/var/run/docker.sock -v $(which docker):/usr/bin/docker -p 8080:8080 moneyjenkins & process_id=$!
+docker run --name money_jenkins -d -v /var/run/docker.sock:/var/run/docker.sock -v $(which docker):/usr/bin/docker -p 8080:8074 moneyjenkins & process_id=$!
 echo " ************************************** "
-echo "Please head to http://localhost:8080 to view Jenkins!"
+echo "Please head to http://localhost:8074 to view Jenkins!"
 echo " ************************************** "
 exit 0
 
