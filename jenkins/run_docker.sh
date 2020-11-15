3296c912f36a306d2fbf581a53995792c592731d
alejandrojsuazohvd <als5289@g.harvard.edu>
2020-11-15 10:19:06 -0600

    Using external port 8085

--- jenkins/run_docker.sh
+++ jenkins/run_docker.sh
@@ -2,7 +2,7 @@
 docker stop money_jenkins
 docker rm -v money_jenkins
 docker build -t moneyjenkins .
-docker run --name money_jenkins -d -v /var/run/docker.sock:/var/run/docker.sock -v $(which docker):/usr/bin/docker -p 8080:8085 moneyjenkins & process_id=$!
+docker run --name money_jenkins -d -v /var/run/docker.sock:/var/run/docker.sock -v $(which docker):/usr/bin/docker -p 8085:8080 moneyjenkins & process_id=$!
 echo " ************************************** "
 echo "Please head to http://localhost:8085 to view Jenkins!"
 echo " ************************************** "
