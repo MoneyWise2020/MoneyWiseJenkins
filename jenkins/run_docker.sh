a49c99e912e9b152e5c87471328536ed05265d16
alejandrojsuazohvd <als5289@g.harvard.edu>
2020-11-15 09:56:24 -0600

    Updating docker install to include docker-compose

--- jenkins/run_docker.sh
+++ jenkins/run_docker.sh
@@ -2,9 +2,9 @@
 docker stop money_jenkins
 docker rm -v money_jenkins
 docker build -t moneyjenkins .
-docker run --name money_jenkins -d -v /var/run/docker.sock:/var/run/docker.sock -v $(which docker):/usr/bin/docker -p 8080:8074 moneyjenkins & process_id=$!
+docker run --name money_jenkins -d -v /var/run/docker.sock:/var/run/docker.sock -v $(which docker):/usr/bin/docker -p 8080:8085 moneyjenkins & process_id=$!
 echo " ************************************** "
-echo "Please head to http://localhost:8074 to view Jenkins!"
+echo "Please head to http://localhost:8085 to view Jenkins!"
 echo " ************************************** "
 exit 0
 
